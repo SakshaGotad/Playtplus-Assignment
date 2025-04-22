2db06886c0d58482392f87b470f9691caa08c6f9
SakshaGotad <sakshagotad@gmail.com>
2025-04-22 17:06:32 +0530

    authentication controller logic updated

--- routes/events.js
+++ routes/events.js
@@ -1,4 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const authControllers = require('../controller/authController');
+
+router.route('/register').post(authControllers.register);
+router.route('/login').post(authControllers.login);
+
 
 module.exports = router;
